995c2e185ecb1a09e8fe9ed52df480a2b07f8585
vaibhavmore18 <153914758+vaibhavmore18@users.noreply.github.com>
2023-12-29 12:07:50 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Java Program...");  
         System.out.println("Output from java program");
-        System.out.println(new Date());       
+        System.out.println(new Date()); 
+        System.out.println("hello");      
     }
 }
