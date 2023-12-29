417a7ab199aa1010f3d14e81f487befe9f297128
vaibhavmore18 <153914758+vaibhavmore18@users.noreply.github.com>
2023-12-29 12:03:56 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Java Program...");  
+        System.out.println("Output from java program");
+        System.out.println(new Date());       
+    }
+}
